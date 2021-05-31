a5e3de1bdc38776e1f8bb800b1bcab7b363e4af5
Elison Liu <elisonliu97@gmail.com>
2021-05-31 13:11:15 -0400

    element selectors in script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// ELEMENT SELECTORS
+var userFormEl = document.querySelector('#user-form');
+var cityButtonsEl = document.querySelector('#city-buttons');
+var cityInputEl = document.querySelector('#city-input');
+var weatherSearchTerm = document.querySelector('#weather-search-term');
+var weatherContainerEl = document.querySelector('#weather-container');
+var fiveDayContainerEl = document.querySelector('#five-day-container')
