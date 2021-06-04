84c0694c1738cc7e6fbb83bdb5fd5d9cf853035a
Elison Liu <elisonliu97@gmail.com>
2021-06-03 21:45:32 -0400

    created sections and added classes in html file

--- script.js
+++ script.js
@@ -4,4 +4,15 @@ var cityButtonsEl = document.querySelector('#city-buttons');
 var cityInputEl = document.querySelector('#city-input');
 var weatherSearchTerm = document.querySelector('#weather-search-term');
 var weatherContainerEl = document.querySelector('#weather-container');
-var fiveDayContainerEl = document.querySelector('#five-day-container')
+var fiveDayContainerEl = document.querySelector('#five-day-container');
+
+// function formSubmitHandler(event) {
+//     event.preventDefault();
+
+//     var cityName = cityInputEl.value.trim();
+
+//     if (cityName
+// }
+
+userFormEl.addEventListener('submit', formSubmitHandler);
+cityButtonsEl.addEventListener('click', buttonClickHandler);
