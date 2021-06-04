3fe1172bf22253fb0e5e628ba425611f5fca97c7
Elison Liu <elisonliu97@gmail.com>
2021-06-03 22:53:03 -0400

    created fetch request in js file

--- script.js
+++ script.js
@@ -6,13 +6,50 @@ var weatherSearchTerm = document.querySelector('#weather-search-term');
 var weatherContainerEl = document.querySelector('#weather-container');
 var fiveDayContainerEl = document.querySelector('#five-day-container');
 
-// function formSubmitHandler(event) {
-//     event.preventDefault();
+// VARIABLES
+var apiKey = "c19e4da08eb6aeec142736a2f4693638"
+var currentData
+var fiveDayData
 
-//     var cityName = cityInputEl.value.trim();
+function formSubmitHandler(event) {
+    event.preventDefault();
 
-//     if (cityName
-// }
+    var cityName = cityInputEl.value.trim();
 
-userFormEl.addEventListener('submit', formSubmitHandler);
-cityButtonsEl.addEventListener('click', buttonClickHandler);
+    if (!cityName){
+        return;
+    }
+    getWeatherData(cityName)
+}
+
+function getWeatherData(city) {
+    var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey
+
+    fetch(weatherUrl)
+        .then(function (response) {
+            response.json().then(function (data) {
+                currentData = data.main
+            })
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+
+
+}
+
+function getForecastData(lat, lon) {
+    var fiveDayUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&appid=" + apiKey
+    fetch(fiveDayUrl)
+        .then(function (response) {
+            response.json().then(function (data) {
+                fiveDayData = data.daily
+            })
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+
+// userFormEl.addEventListener('submit', formSubmitHandler);
+// cityButtonsEl.addEventListener('click', buttonClickHandler);
